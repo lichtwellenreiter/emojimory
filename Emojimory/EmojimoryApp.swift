d28100c97a4874e8859673f07752ce09914dc8c9
florian <florianjeremias@gmail.com>
2020-10-01 15:14:37 +0200

    init

---
+++ Emojimory/EmojimoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojimoryApp.swift
+//  Emojimory
+//
+//  Created by Florian Thiévent on 01.10.20.
+//
+
+import SwiftUI
+
+@main
+struct EmojimoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
